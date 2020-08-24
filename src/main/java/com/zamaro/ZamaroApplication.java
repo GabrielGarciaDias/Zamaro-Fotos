f3369543018f582ba3981764b9cc07a2518a9ac5
igoryy <igor.siviero1@gmail.com>
2020-08-24 00:32:32 -0300

    Commit inicial

---
+++ src/main/java/com/zamaro/ZamaroApplication.java
@@ -0,0 +1,13 @@
+package com.zamaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZamaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZamaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zamaro/ZamaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zamaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZamaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
